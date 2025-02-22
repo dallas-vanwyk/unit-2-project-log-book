20b28e236870e7e5e460dcf4b1447683f798f6c6
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-02-21 21:04:30 -0600

    working on home page, user model, log note model, user auth

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+// controllers/auth.js
+
+// -------------------------------------------------------------- required modules
+
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// need to fill this out
+const User = require('../models/user.js');
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------- 
--- server.js
+++ server.js
@@ -16,32 +16,53 @@ const session = require('express-session');
 
 // -------------------------------------------------------------- other files...
 
-const authController = require('./controllers/auth.js');
-const isSignedIn = require('/middleware/is-signed-in.js');
-const passUserToView = require('/middleware/pass-user-to-view.js');
-const lognotesController = require('./controllers/lognotes.js');
+// const authController = require('./controllers/auth.js');
+// const isSignedIn = require('/middleware/is-signed-in.js');
+// const passUserToView = require('/middleware/pass-user-to-view.js');
+// const lognotesController = require('./controllers/lognotes.js');
 
+// -------------------------------------------------------------- other parameters
 
+const port = process.env.PORT ? process.env.PORT : "3000";
 
+mongoose.connect(process.env.MONGODB_URI);
 
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
+app.use(express.urlencoded({ extended: false }));
 
+app.use(methodOverride('_method'));
 
-
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 // -------------------------------------------------------------- middleware
 
 
 // -------------------------------------------------------------- routes
 
+app.get('/', (req, res) => {
+    // start at the sign in page
+    res.render('/views/auth/sign-in.ejs');
+
+    // if signed in, go to log notes page
+
+});
+
+
 
 // -------------------------------------------------------------- port listener
 
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}.`);
+});
 
-
-
-
-// -------------------------------------------------------------- 
-// -------------------------------------------------------------- 
 // -------------------------------------------------------------- 
 // -------------------------------------------------------------- 
