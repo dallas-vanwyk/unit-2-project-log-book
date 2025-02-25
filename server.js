73de99d28f1bd147a751e8b88f35a199f4f48578
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-02-25 12:05:26 -0600

    reorganizing server.js for clarity and understanding - consolidating middleware

--- server.js
+++ server.js
@@ -7,17 +7,37 @@ dotenv.config();
 
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
 const mongoose = require('mongoose');
 
 const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
 
 const morgan = require('morgan');
-app.use(morgan('dev'));
 
 const session = require('express-session');
+
+const path = require('path');
+
+const authController = require('./controllers/auth.js');
+
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+// -------------------------------------------------------------- other parameters
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    // deliberately leaving this console.log in place
+    // (not ignoring the project requirements)
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// -------------------------------------------------------------- middleware
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -26,14 +46,13 @@ app.use(
     })
 );
 
-// -------------------------------------------------------------- middleware
+// morgan for logs for debugging
+app.use(morgan('dev'));
 
-// style
-const path = require('path');
+// public folder path for stylesheet
 app.use(express.static(path.join(__dirname, "public")));
 
 // authController for all session authentication, including sign-in page
-const authController = require('./controllers/auth.js');
 app.use('/auth', authController);
 
 // check
@@ -49,27 +68,14 @@ app.use(passUserToView);
 const lognotesController = require('./controllers/lognotes.js');
 app.use('/lognotes', lognotesController)
 
-// -------------------------------------------------------------- other parameters
-
-const port = process.env.PORT ? process.env.PORT : "3000";
-
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
 
 // -------------------------------------------------------------- routes
 
-// the root route; there is no need for a 'home page'
+// the root route directs to the lognote index if signed in
+// there is no need for a 'home page'
+// /index.ejs is unused at this time; maintained for possible future use
 app.get('/', (req, res) => {
-    if (req.session.user) {
-        // if signed in, go to log notes page
-        res.redirect('/lognotes');
-    } else {
-        // start at the sign in page
-        res.redirect('/auth/sign-in.ejs');
-    };
+    res.redirect('/lognotes');
 });
 
 // -------------------------------------------------------------- port listener
